24f54212a15eef69cb397f80797a9945d5c3b453
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2020-12-18 21:14:33 +0100

    Three tier sponsors

--- tools/make_sponsors.py
+++ tools/make_sponsors.py
@@ -4,24 +4,42 @@ from jinja2 import Template
 
 data = yaml.safe_load(sys.stdin)
 
+for entry in data:
+    if not entry.get("website"):
+        entry["website"] = f"https://github.com/{entry['username']}"
+
 template = Template("""\
-<div class="sponsors-avatars-top">
+<div class="sponsors-avatars sponsors-avatars-top">
 {%- for entry in data %}
     {%- if entry.tier == "top" %}
-    <div class="sponsors-avatar-top">
-        <a href="https://github.com/{{entry.username}}">
+    <div class="sponsors-avatar">
+        <a href="{{entry.website}}">
             <img class="avatar" src="{{entry.avatar}}" title="{{entry.name}}"/>
+            <div class="sponsor-name">{{entry.name}}</div>
         </a>
     </div>
     {%- endif %}
 {%- endfor %}
 </div>
 
-<div class="sponsors-avatars-bottom">
+<div class="sponsors-avatars sponsors-avatars-mid">
 {%- for entry in data %}
-    {%- if entry.tier != "top" %}
-    <div class="sponsors-avatar-bottom">
-        <a href="https://github.com/{{entry.username}}">
+    {%- if entry.tier == "mid" %}
+    <div class="sponsors-avatar">
+        <a href="{{entry.website}}">
+            <img class="avatar" src="{{entry.avatar}}" title="{{entry.name}}"/>
+            <div class="sponsor-name">{{entry.name}}</div>
+        </a>
+    </div>
+    {%- endif %}
+{%- endfor %}
+</div>
+
+<div class="sponsors-avatars sponsors-avatars-bottom">
+{%- for entry in data %}
+    {%- if not entry.tier %}
+    <div class="sponsors-avatar">
+        <a href="{{entry.website}}">
             <img class="avatar" src="{{entry.avatar}}" title="{{entry.name}}"/>
         </a>
     </div>
@@ -29,51 +47,5 @@ template = Template("""\
 {%- endfor %}
 </div>
 """)
-sys.stdout.write(template.render(data=data))
 
-'''
-<div class="sponsors-avatars-top docutils container">
-<div class="sponsors-avatar-top docutils container">
-<img alt="../img/sponsors/top/601732.png" class="avatar" src="../img/sponsors/top/601732.png" />
-</div>
-<div class="sponsors-avatar-top docutils container">
-<img alt="../img/sponsors/top/37837.png" class="avatar" src="../img/sponsors/top/37837.png" />
-</div>
-<div class="sponsors-avatar-top docutils container">
-<img alt="../img/sponsors/top/3852020.jpeg" class="avatar" src="../img/sponsors/top/3852020.jpeg" />
-</div>
-<div class="sponsors-avatar-top docutils container">
-<img alt="../img/sponsors/top/330373.png" class="avatar" src="../img/sponsors/top/330373.png" />
-</div>
-</div>
-<div class="sponsors-avatars-bottom docutils container">
-<div class="sponsors-avatar-bottom docutils container">
-<img alt="../img/sponsors/94721.jpeg" class="avatar" src="../img/sponsors/94721.jpeg" />
-</div>
-<div class="sponsors-avatar-bottom docutils container">
-<img alt="../img/sponsors/115712.jpeg" class="avatar" src="../img/sponsors/115712.jpeg" />
-</div>
-<div class="sponsors-avatar-bottom docutils container">
-<img alt="../img/sponsors/174182.png" class="avatar" src="../img/sponsors/174182.png" />
-</div>
-<div class="sponsors-avatar-bottom docutils container">
-<img alt="../img/sponsors/2050405.png" class="avatar" src="../img/sponsors/2050405.png" />
-</div>
-<div class="sponsors-avatar-bottom docutils container">
-<img alt="../img/sponsors/3085224.png" class="avatar" src="../img/sponsors/3085224.png" />
-</div>
-<div class="sponsors-avatar-bottom docutils container">
-<img alt="../img/sponsors/7447491.jpeg" class="avatar" src="../img/sponsors/7447491.jpeg" />
-</div>
-<div class="sponsors-avatar-bottom docutils container">
-<img alt="../img/sponsors/7826876.jpeg" class="avatar" src="../img/sponsors/7826876.jpeg" />
-</div>
-<div class="sponsors-avatar-bottom docutils container">
-<img alt="../img/sponsors/14254614.png" class="avatar" src="../img/sponsors/14254614.png" />
-</div>
-<div class="sponsors-avatar-bottom docutils container">
-<img alt="../img/sponsors/16618300.jpeg" class="avatar" src="../img/sponsors/16618300.jpeg" />
-</div>
-</div>
-</div>
-'''
+sys.stdout.write(template.render(data=data))
